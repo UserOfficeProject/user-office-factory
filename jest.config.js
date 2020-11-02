d8551fcfe38df788f99c3b0ed23d98d1a4bdd820
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2020-11-02 09:34:11 +0100

    jest config cleanup

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,4 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/src/pdf/pdfTableOfContents/*.js',
-  ],
 };
