d5dbd69480ee39aa082bb7105a78da8beecbb260
Panda (Thomas) Attwood <thomas.attwood@stfc.ac.uk>
2022-08-24 11:27:47 +0100

    Add debug helper comments

--- src/template/helpers/debug.ts
+++ src/template/helpers/debug.ts
@@ -4,8 +4,19 @@ import handlebar from 'handlebars';
 
 const regex = new RegExp(EOL, 'g');
 
-handlebar.registerHelper('$debug', function (value: unknown) {
-  return new handlebar.SafeString(
-    JSON.stringify(value, null, 2).replace(regex, '<br>')
-  );
-});
+handlebar.registerHelper(
+  '$debug',
+  /**
+   * Takes the value and outputs it as JSON with HTML line breaks to make
+   * debugging templates easier for template designers.
+   *
+   * Used as follows:
+   *
+   * {{$debug <value>}}
+   */
+  function (value: unknown) {
+    return new handlebar.SafeString(
+      JSON.stringify(value, null, 2).replace(regex, '<br>')
+    );
+  }
+);
