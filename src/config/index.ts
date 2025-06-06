5dc24c345d06e68b4c0bd1056f873a305d6caa95
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-06-06 16:06:43 +0100

    remove changes not needed

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-//import 'reflect-metadata';
-import './default/dependencyConfig';
-
-//Import custom dependence if set.
-if (process.env.DEPENDENCY_CONFIG == 'stfc') {
-  require('./stfc/dependencyConfig');
-}
-
-export {};
